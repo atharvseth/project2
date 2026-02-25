d29b6cd2c3de4b99f5872db19787d09ee9b67187
atharvseth <Atharvabhiseth@gmail.com>
2026-02-25 18:49:09 +0530

    project

---
+++ profect3.py
@@ -0,0 +1,6 @@
+a  = input("enter a number to check if it is even or odd: ")
+if int(a) % 2 == 0: 
+    print("the number is even")
+else:
+    print("the number is odd")
+
---
+++ project1.py
@@ -0,0 +1,10 @@
+# making a calender
+import calendar
+yy = 2026
+mm = 1
+print(calendar.month(yy, mm))
+
+
+
+
+
